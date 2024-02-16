6e9922b56bf15a49e4281e2b5b122f3906909e52
99Rafick <rafraf9727@gmail.com>
2024-02-16 10:36:04 +0100

    Custom: v 1.0

--- custom_translate.js
+++ custom_translate.js
@@ -1,6 +1,17 @@
+
+function ind() {
+    const defaultLanguage = navigator.language || navigator.browserLanguage;
+    return defaultLanguage.split('-')[0];
+}
+
 function googleTranslateElementInit() {
     new google.translate.TranslateElement(
-        {pageLanguage: 'en'},
+        {pageLanguage: ind(), includedLanguages: 'fr,en,es,de,it'},
         'google_translate_element'
     );
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const d = document.querySelector('.goog-te-combo')
+    console.log(d)
+})
